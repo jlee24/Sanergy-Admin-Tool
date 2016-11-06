4f8a26ced25882e29e86f4c653446846e70b32c5
jihyeonlee1 <janellee0@gmail.com>
2016-11-06 13:21:41 -0800

    testing

--- sanergyadmin/settings.py
+++ sanergyadmin/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'salesforce'
+    #'salesforce'
 ]
 
 MIDDLEWARE_CLASSES = [
