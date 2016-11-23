9ae319a95a37a492e5d9ae7ab8674ed97d004c43
jlee24 <jlee24@stanford.edu>
2016-11-22 20:38:12 -0800

    trying angular

--- calendarApp/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("This is the Calendar App Index.")
-# Create your views here.
